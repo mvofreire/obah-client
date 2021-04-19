7590cc32afa7edb10fcc46eb477d25780047eab5
Marcus Freire <vinny.freire@gmail.com>
2021-04-19 14:13:45 -0300

    add logs

--- src/config/index.js
+++ src/config/index.js
@@ -1,6 +1,10 @@
 const isProd = process.env.NODE_ENV === "production";
+const userSessionKey = isProd ? 'obah-web-session-key' : 'obah-web-session-key-local'
+const baseUrl = process.env.REACT_APP_API_URL;
+
+console.log(baseUrl, userSessionKey);
 
 export default {
-  userSessionKey: `obah-web-session-key-${isProd ? "" : "-local"}`,
-  baseUrl: process.env.REACT_APP_API_URL,
+  userSessionKey,
+  baseUrl
 };
